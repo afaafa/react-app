c3cf98c05bcd3cd224777fbf35bdef4d8f70f543
afaafa <afhazachra@gmail.com>
2021-02-20 23:15:26 +0700

    create folder component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import './stylesheet.css';
+import '../stylesheet.css';
 
 class App extends React.Component {
   render() {
